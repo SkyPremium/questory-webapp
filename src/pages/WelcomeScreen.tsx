640c3ef9a5eede263710d2c8e8cf25cb9dd34efe
SkyPremium <48849623+SkyPremium@users.noreply.github.com>
2025-05-18 17:48:19 +1000

    Add files via upload

--- src/pages/WelcomeScreen.tsx
+++ src/pages/WelcomeScreen.tsx
@@ -1,32 +1,32 @@
-// src/pages/WelcomeScreen.tsx
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import welcomeBg from "../assets/images/welcome.jpg"; // путь к картинке в src
 
 const WelcomeScreen: React.FC = () => {
   const navigate = useNavigate();
 
-  const handleStartClick = () => {
-    navigate("/menu");
+  const handleStart = () => {
+    navigate("/rules"); // Переход ко второй сцене
   };
 
   return (
-    <div className="relative w-full h-full">
-      {/* Фоновое изображение на всю сцену */}
+    <div className="relative w-full h-screen overflow-hidden">
+      {/* Фоновое изображение */}
       <img
-        src="/assets/images/welcome.jpg"
-        alt="Welcome"
+        src={welcomeBg}
+        alt="Questory Welcome"
         className="w-full h-full object-cover"
       />
 
-      {/* Невидимая кнопка поверх кнопки на изображении */}
-      <button
-        onClick={handleStartClick}
-        className="absolute bottom-[10%] left-1/2 -translate-x-1/2 w-[180px] h-[60px] rounded-xl focus:outline-none"
-        style={{ backgroundColor: "transparent" }}
+      {/* Невидимая кнопка с эффектом нажатия */}
+      <div
+        onClick={handleStart}
+        className="absolute bottom-[80px] left-1/2 transform -translate-x-1/2 w-[200px] h-[60px] cursor-pointer rounded-lg transition-all duration-150 active:scale-95 active:bg-black/20"
+        title="Начать"
       >
-        {/* Можно добавить эффект наведения */}
+        {/* Для доступности, но визуально скрыто */}
         <span className="sr-only">Начать</span>
-      </button>
+      </div>
     </div>
   );
 };
