a8eef0fc04a69997c25e65b16e2f152ef4fbc896
SkyPremium <48849623+SkyPremium@users.noreply.github.com>
2025-05-18 18:38:08 +1000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   build: {
+    outDir: 'dist',
     rollupOptions: {
-      input: 'index.html'
-    }
+      input: 'index.html',
+    },
+  },
+  server: {
+    historyApiFallback: true, // это важно!
   }
-})
+});
