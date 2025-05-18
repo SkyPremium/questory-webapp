9a87d1b4f415d9f46a0fd7ed4512ccf436f13804
SkyPremium <48849623+SkyPremium@users.noreply.github.com>
2025-05-18 17:46:09 +1000

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+// src/App.tsx
+
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomeScreen from "./pages/WelcomeScreen";
+import RulesScreen from "./pages/RulesScreen";
+// Подключи другие экраны позже (TrainingScreen, MainMenu и т.д.)
 
 function App() {
-  return <WelcomeScreen />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+        <Route path="/rules" element={<RulesScreen />} />
+        {/* Добавишь позже:
+        <Route path="/training" element={<TrainingScreen />} />
+        <Route path="/menu" element={<MainMenu />} />
+        */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
