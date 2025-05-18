3a6c27ebf881b1633720c728f9f4e70665a70fc2
SkyPremium <48849623+SkyPremium@users.noreply.github.com>
2025-05-18 18:33:44 +1000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import react from '@vitejs/plugin-react';
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    strictPort: true,
-    port: 5173,
-    allowedHosts: "all"
+    historyApiFallback: true
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
   }
-}
+})
