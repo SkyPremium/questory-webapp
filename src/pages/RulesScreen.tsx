2177fbc1069c787157e8a5a53c0248b9df49e861
SkyPremium <48849623+SkyPremium@users.noreply.github.com>
2025-05-20 00:55:43 +1000

    Add files via upload

--- src/pages/RulesScreen.tsx
+++ src/pages/RulesScreen.tsx
@@ -11,7 +11,7 @@ export default function RulesScreen({ onAgree }: { onAgree: () => void }) {
   return (
     <div className="relative w-full h-screen flex justify-center items-center bg-black overflow-hidden">
 
-      {/* Левая размытие-зона */}
+      {/* 🌫 Левая размытие-зона */}
       <div className="absolute left-0 top-0 bottom-0 w-[max(0px,calc(50%-384px))] z-0 overflow-hidden">
         <img
           src={rulesBg}
@@ -20,7 +20,7 @@ export default function RulesScreen({ onAgree }: { onAgree: () => void }) {
         />
       </div>
 
-      {/* Правая размытие-зона */}
+      {/* 🌫 Правая размытие-зона */}
       <div className="absolute right-0 top-0 bottom-0 w-[max(0px,calc(50%-384px))] z-0 overflow-hidden">
         <img
           src={rulesBg}
@@ -29,28 +29,46 @@ export default function RulesScreen({ onAgree }: { onAgree: () => void }) {
         />
       </div>
 
-      {/* Центральный 3:4 фрейм */}
+      {/* 🌫 Верхнее размытие-зона */}
+      <div className="absolute top-0 left-0 right-0 h-[max(0px,calc(50%-512px))] z-0 overflow-hidden">
+        <img
+          src={rulesBg}
+          className="w-full h-full object-cover blur-md scale-125 opacity-40"
+          alt=""
+        />
+      </div>
+
+      {/* 🌫 Нижнее размытие-зона */}
+      <div className="absolute bottom-0 left-0 right-0 h-[max(0px,calc(50%-512px))] z-0 overflow-hidden">
+        <img
+          src={rulesBg}
+          className="w-full h-full object-cover blur-md scale-125 opacity-40"
+          alt=""
+        />
+      </div>
+
+      {/* 📜 Центральный контейнер 3:4 */}
       <div className="relative w-full max-w-[768px]" style={{ aspectRatio: "3 / 4" }}>
-        {/* 📜 Основной фон */}
+        {/* Фон */}
         <img
           src={rulesBg}
           alt="Правила"
           className="absolute inset-0 w-full h-full object-cover z-0"
         />
 
-        {/* UI */}
+        {/* UI-контейнер */}
         <div className="absolute inset-0 z-10 flex flex-col justify-between">
           {/* 🔥 Логотип */}
           <div className="pt-[2%] flex justify-center">
             <img src={logo} alt="Questory Logo" className="w-[130px]" />
           </div>
 
-          {/* ✅ Галочка + кнопка */}
+          {/* ✅ Галочка и кнопка */}
           <div className="pb-[3%] flex flex-col items-center gap-4">
             <div onClick={() => setChecked(!checked)} className="cursor-pointer">
               <img
                 src={checked ? checkboxChecked : checkboxEmpty}
-                alt="Печать"
+                alt="Галочка"
                 className="w-[52px]"
               />
             </div>
