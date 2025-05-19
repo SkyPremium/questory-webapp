59bd0bab24db51697872ef9512545b62286d1763
SkyPremium <48849623+SkyPremium@users.noreply.github.com>
2025-05-20 01:28:33 +1000

    Add files via upload

--- src/pages/RulesScreen.tsx
+++ src/pages/RulesScreen.tsx
@@ -11,36 +11,36 @@ export default function RulesScreen({ onAgree }: { onAgree: () => void }) {
   return (
     <div className="relative w-full h-screen flex justify-center items-center bg-black overflow-hidden">
 
-      {/* 🌫 Размытый задний фон, адаптированный по цвету */}
+      {/* 🌫 Размытый задний фон (растянутый на экран) */}
       <img
         src={rulesBg}
         alt="Размытый фон"
         className="absolute inset-0 w-full h-full object-cover blur-[40px] scale-110 brightness-[1.1] saturate-[1.1] opacity-70 z-0"
       />
 
-      {/* 📜 Центральный контейнер 3:4 */}
-      <div className="relative w-full max-w-[768px] aspect-[3/4] z-10">
-        {/* Основная чёткая сцена */}
+      {/* 🧱 Центральный блок 1080×1920 с aspect-[9/16] */}
+      <div className="relative w-full max-w-[1080px] aspect-[9/16] z-10">
+        {/* Чёткое изображение сцены */}
         <img
           src={rulesBg}
           alt="Фон сцены"
           className="absolute inset-0 w-full h-full object-cover z-10"
         />
 
-        {/* UI-элементы поверх */}
+        {/* UI-контейнер */}
         <div className="absolute inset-0 z-20 flex flex-col justify-between">
           {/* 🔥 Логотип */}
-          <div className="pt-[2%] flex justify-center">
-            <img src={logo} alt="Questory Logo" className="w-[130px]" />
+          <div className="pt-[4%] flex justify-center">
+            <img src={logo} alt="Questory Logo" className="w-[140px]" />
           </div>
 
           {/* ✅ Галочка и кнопка */}
-          <div className="pb-[3%] flex flex-col items-center gap-4">
+          <div className="pb-[5%] flex flex-col items-center gap-4">
             <div onClick={() => setChecked(!checked)} className="cursor-pointer">
               <img
                 src={checked ? checkboxChecked : checkboxEmpty}
                 alt="Галочка"
-                className="w-[52px]"
+                className="w-[54px]"
               />
             </div>
 
@@ -51,7 +51,7 @@ export default function RulesScreen({ onAgree }: { onAgree: () => void }) {
                 checked ? "opacity-100" : "opacity-40 pointer-events-none"
               }`}
             >
-              <img src={buttonAgree} alt="Согласен" className="w-[140px]" />
+              <img src={buttonAgree} alt="Согласен" className="w-[150px]" />
             </button>
           </div>
         </div>
