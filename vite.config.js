80d404e2a3d6ec6223a4efa94a3ae6ba0f5cc31f
SkyPremium <48849623+SkyPremium@users.noreply.github.com>
2025-05-21 22:54:45 +1000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   plugins: [react()],
 });
