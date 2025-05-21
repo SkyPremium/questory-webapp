efe946e776d1014a5a4140a5cfdcbac05d8c5991
SkyPremium <48849623+SkyPremium@users.noreply.github.com>
2025-05-21 17:14:24 +1000

    Add files via upload

--- src/pages/LoadingScreen.tsx
+++ src/pages/LoadingScreen.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import loadingBg from "@/assets/images/loading.jpg";
+import loadingBg from "../assets/images/loading.jpg";
 
 export default function LoadingScreen() {
   const navigate = useNavigate();
